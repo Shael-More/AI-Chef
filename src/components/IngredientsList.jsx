dc5c30035d99b1fa8c2dc8e691740fe281615681
Shael more <89303457+Shael-More@users.noreply.github.com>
2024-11-23 18:25:49 +0530

    Add ingredientList file

---
+++ src/components/IngredientsList.jsx
@@ -0,0 +1,29 @@
+export default function IngredientsList(props) {
+  const ingredientsList = props.ingredients.map((item) => <li key={item}>{item}</li>);
+
+  return (
+    <section className='p-10 font-mono space-y-4'>
+      <h2 className='text-3xl  font-bold'>Ingredients on hand:</h2>
+      <ul className='list-disc ml-5' aria-live='polite'>
+        {ingredientsList}
+      </ul>
+      {props.ingredients.length > 3 && (
+        <div className='flex text-sm bg-slate-200 p-5 justify-between rounded-lg items-center max-w-lg mx-auto'>
+          <div className='space-y-5 '>
+            <h3 className='font-bold text-lg '>Ready for a recipe?</h3>
+            <p className='text-gray-500 text-sm'>
+              Generate a recipe from your list of ingredients
+            </p>
+          </div>
+
+          <button
+            onClick={props.toggleRecipeShown}
+            className='bg-[#6f6866] text-base text-white p-2 rounded-md'
+          >
+            Get a recipe
+          </button>
+        </div>
+      )}
+    </section>
+  );
+}
